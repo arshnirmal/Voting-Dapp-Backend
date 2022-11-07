1326dd217d468d3ca4c3ae470ba0651848a081a7
Arsh Nirmal <arshnirmal11@gmail.com>
2022-11-08 03:08:29 +0530

    gsn

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,58 +1,14 @@
 //SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 import "hardhat/console.sol";
-contract CollegeContract {
+import "@openzeppelin/contracts/metatx/ERC2771Context.sol";
+import "@openzeppelin/contracts/metatx/MinimalForwarder.sol";
+
+
+contract CollegeContract is ERC2771Context {
     string public quote = "sfvkdjfbvjlvn";
     address public owner;
     
-    struct EIP712Domain {
-        string name;
-        string version;
-        uint256 chainId;
-        address verifyingContract;
-    }
-
-    struct MetaTransaction {
-            uint256 nonce;
-            address from;
-    }
-    mapping(address => uint256) public nonces;
-
-    bytes32 internal constant EIP712_DOMAIN_TYPEHASH = keccak256(bytes("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"));
-    bytes32 internal constant META_TRANSACTION_TYPEHASH = keccak256(bytes("MetaTransaction(uint256 nonce,address from)"));
-    bytes32 internal DOMAIN_SEPARATOR = keccak256(abi.encode(
-        EIP712_DOMAIN_TYPEHASH,
-            keccak256(bytes("Quote")),
-            keccak256(bytes("1")),
-            5, // goerli
-            address(this)
-    ));
-
-function setQuoteMeta(address userAddress,string memory newQuote, bytes32 r, bytes32 s, uint8 v) public {
-        
-    MetaTransaction memory metaTx = MetaTransaction({
-        nonce: nonces[userAddress],
-        from: userAddress
-    });
-    
-    bytes32 digest = keccak256(
-        abi.encodePacked(
-                "\x19\x01",
-                DOMAIN_SEPARATOR,
-                keccak256(abi.encode(META_TRANSACTION_TYPEHASH, metaTx.nonce, metaTx.from))
-        )
-    );
-
-   require(userAddress != address(0), "invalid-address-0");
-   require(userAddress == ecrecover(digest, v, r, s), "invalid-signatures");
-	
-   quote = newQuote;
-   owner = userAddress;
-   nonces[userAddress]++;
- }       // Make an instance of the predefined struct
-  
-//<-- Auth
-    // address of the contract deployer.
     mapping(address => bool) private admins;
     string allowedUser = "ipfsLink";
     
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,34 +4,49 @@ const {
   } = require('defender-relay-client/lib/ethers')
   const { ethers } = require('hardhat')
   const { writeFileSync } = require('fs')
-
-async function main(){
-    const [deployer] = await ethers.getSigners();
-    console.log("Contract Deployed by"+ deployer.address);
-    const contract = await ethers.getContractFactory("CollegeContract");
-    const token = await contract.deploy();
-    console.log("Token address (Contract Address):", token.address);
-    console.log(token)
-}
-
+  
+  async function main() {
+    require('dotenv').config()
+    const credentials = {
+      apiKey: process.env.RELAYER_API_KEY,
+      apiSecret: process.env.RELAYER_API_SECRET,
+    }
+    const provider = new DefenderRelayProvider(credentials)
+    const relaySigner = new DefenderRelaySigner(credentials, provider, {
+      speed: 'safeLow',
+    })
+  
     const Forwarder = await ethers.getContractFactory('MinimalForwarder')
     const forwarder = await Forwarder.connect(relaySigner)
-        .deploy()
-        .then((f) => f.deployed())
-
+      .deploy()
+      .then((f) => f.deployed())
+  
+    const Token = await ethers.getContractFactory('CollegeContract')
+    const token = await Token.connect(relaySigner)
+      .deploy(forwarder.address)
+      .then((f) => f.deployed())
+  
     writeFileSync(
-        'deploy.json',
-        JSON.stringify(
+      'deploy.json',
+      JSON.stringify(
         {
-            MinimalForwarder: forwarder.address,    
+          MinimalForwarder: forwarder.address,          
         },
         null,
         2
-        )
+      )
     )
-
+  
     console.log(
-        `MinimalForwarder: ${forwarder.address}`
-    )   
-
-main().then(()=>process.exit(0)).catch((err)=>{console.log(err);process.exit(1)})
+      `MinimalForwarder: ${forwarder.address}`
+    )
+  }
+  
+  if (require.main === module) {
+    main()
+      .then(() => process.exit(0))
+      .catch((error) => {
+        console.error(error)
+        process.exit(1)
+      })
+  }
